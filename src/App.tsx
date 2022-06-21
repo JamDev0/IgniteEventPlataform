43dbf7d37df38a4b95ae43b7cea2c1a102c5be26
JAMDev <jamdeveloper0@gmail.com>
2022-06-21 02:15:57 -0300

    Teste GraphQl
    Código feito apenas para que eu entenda melhor o graphql e o apollo.
    
    Ele pede á api os titulos e ids das aulas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { gql, useQuery } from "@apollo/client"
+
+const LESSONS_TITLE_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface lessonsInterface {
+  id: string;
+  title: string
+}
+
 export function App() {
+  const { data } = useQuery<{ lessons: lessonsInterface[] }>(LESSONS_TITLE_QUERY)
 
   return (
-    <>
-      
-    </>
+    <ul>
+      {
+        data?.lessons.map(lesson => <li key={lesson.id}>{lesson.title}</li>)
+      }
+    </ul>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { ApolloProvider } from '@apollo/client';
+import { client } from './libs/apollo';
+
 import { App } from './App';
+
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
