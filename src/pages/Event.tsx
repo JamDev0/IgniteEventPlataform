917474c45be5ba42ebbe74d6c1d485c5cf96675a
JAMDev <jamdeveloper0@gmail.com>
2022-06-22 02:18:33 -0300

    Criação da página Envent
    Após a separação do app em páginas foi criado a página de eventos

---
+++ src/pages/Event.tsx
@@ -0,0 +1,15 @@
+import { Body } from "../components/Body";
+import { Header } from "../components/Header";
+
+export function Event() {
+    return(
+        <div
+         className="
+            flex flex-col min-h-screen
+         "
+        >
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
