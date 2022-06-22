5916c64b9aa346f90ba88e08c6116381c18ccc00
JAMDev <jamdeveloper0@gmail.com>
2022-06-22 02:18:20 -0300

    Separação do app em páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-
-const LESSONS_TITLE_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface lessonsInterface {
-  id: string;
-  title: string
-}
+import { Event } from "./pages/Event";
 
 export function App() {
-  const { data } = useQuery<{ lessons: lessonsInterface[] }>(LESSONS_TITLE_QUERY)
-
   return (
-    <ul>
-      {
-        data?.lessons.map(lesson => <li key={lesson.id}>{lesson.title}</li>)
-      }
-    </ul>
+    <>
+      <Event/>
+    </>
   )
 }
