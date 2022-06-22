a35e831c2801ec31a93db6b626c3ed2f806d11c1
JAMDev <jamdeveloper0@gmail.com>
2022-06-22 02:14:06 -0300

    Definição do tema do app
    Criação do thema do app nas configurações do tailwind, com todas as fontes do app e cores.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**.tsx"
+    "./src/**/*.tsx"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand': {
+          'green': {
+            '400': '#00B37E',
+            '500': '#00875F',
+            '600': '#015F43'
+          },
+
+          'blue': {
+            '300': '#81D8F7',
+          },
+
+          'orange': {
+            '300': '#FBA94C',
+          },
+
+          'red': {
+            '300': '#F75A68',
+          }
+        },
+
+        'base': {
+          'background': '#09090A',
+          'element': '#121214',
+          'bars': '#121214',
+          'divisor': '#323238',
+          'text': {
+            'title': '#E1E1E6',
+            'body': '#C4C4CC',
+            'secondaryBody': '#8D8D99',
+          }
+        }
+      },
+
+      fontFamily: {
+        'roboto': 'Roboto',
+      }
+    },
   },
   plugins: [],
 }
