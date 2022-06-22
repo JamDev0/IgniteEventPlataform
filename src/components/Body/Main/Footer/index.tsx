2d06d74bca291416521eaca82c8ade381468aff5
JAMDev <jamdeveloper0@gmail.com>
2022-06-22 02:20:25 -0300

    Criação do componente Footer

---
+++ src/components/Body/Main/Footer/index.tsx
@@ -0,0 +1,34 @@
+import RocketLogo from '../../../../assets/RocketLogo.svg';
+
+export function Footer() {
+    return(
+        <footer
+         className="
+            flex justify-between w-full pt-[26px] border-t border-solid border-t-base-divisor text-base text-base-text-secondaryBody mt-[100px]
+         "
+        >
+            <div
+             className='
+                flex gap-x-6
+             '
+            >
+                <img
+                 src={RocketLogo}
+                 className='
+
+                 '
+                />
+
+                <span>
+                    Rocketseat - Todos os direitos reservados
+                </span>
+            </div>
+
+            <a
+             href="#"
+            >
+                Políticas de privacidade
+            </a>
+        </footer>
+    )
+}
